2f4364d8b670dc3a2e08981c992a6d23217204c4
Simon Walz <contact@simonwalz.de>
2021-04-02 12:46:39 +0200

    Refactored module

--- index.js
+++ index.js
@@ -4,31 +4,26 @@ exports.init = function(node, app_config, main, host_info) {
 	if (typeof app_config.pin !== "number") {
 		throw new Error("gpio: pin not defined.");
 	}
-	var not_invert = true;
-	if (typeof app_config.invert) {
-		not_invert = false;
-	}
+	var invert = !!app_config.invert;
 
 	var pin = app_config.pin;
 	var mode = gpio.DIR_OUT;
 	var value = 0;
-	if (app_config.initial == 1) {
-		mode = gpio.DIR_HIGH;
-		value = 1;
-	}
-	if (app_config.initial == 0) {
-		mode = gpio.DIR_LOW;
+	if (typeof app_config.initial_value === "number") {
+		if (app_config.initial_value ^ invert) {
+			mode = gpio.DIR_HIGH;
+			value = 1;
+		} else {
+			mode = gpio.DIR_LOW;
+		}
 	}
 	gpio.setup(pin, mode, function(err) {
 		if (err) throw err;
 	});
 
-	node.announce([{
-		"type": "output.state"
-	}, app_config.metadata]);
 	node.publish(undefined, value);
 	node.rpc_set = function(reply, value, time) {
-		gpio.write(pin, value ^ not_invert, function(err) {
+		gpio.write(pin, value ^ invert, function(err) {
 			if (err) return reply(err);
 
 			node.publish(time, value);
@@ -38,16 +33,14 @@ exports.init = function(node, app_config, main, host_info) {
 	node.rpc_publish = function(reply, time, value) {
 		return this.rpc_set(reply, value, time);
 	};
+	node.announce([{
+		"type": "output.state"
+	}, app_config.metadata]);
 
 	return [
 		node,
 		function() {
-			/*
-			// There is no unexport function:
-			gpio.setup(pin, gpio.DIR_IN, function(err) {
-				if (err) throw err;
-			});
-			*/
+			gpio.destroy();
 		}
 	];
 };
